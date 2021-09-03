c09c33c598e0c564cedcd4244a3d8de8649dc82c
AndreaNathalia <andreareyes@ufm.edu>
2021-09-03 15:28:27 -0600

    add login-signup

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import login
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# render login / index
+@app.route('/', methods =['GET', 'POST'])
+def logIn():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if(login.dbUsuarios['username'] == username and login.dbUsuarios['password'] == password):
+            print(login.dbUsuarios)
+            return render_template('index.html')
+    
+    else:
+        return render_template('login.html')
+
+#render signup
+@app.route('/signup', methods =['GET', 'POST'])
+def signup():
+    if request.method=='POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        login.signup(username, email, password)
+        return render_template('login.html')
+    
+    else:
+        return render_template('signup.html')
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ login.py
@@ -0,0 +1,18 @@
+#dicccionario para simular la base de datos
+dbUsuarios = {"username": "", "email": "", "password": ""}
+
+#Funcion para registrar un nuevo usuario
+def signup(username, email, password):
+    dbUsuarios["username"] = username
+    dbUsuarios["email"] = email
+    dbUsuarios["password"] = password
+
+    print(dbUsuarios)
+
+
+#Funcion para ingresar a home
+def login(username, password):
+    if(dbUsuarios['username'] == username and dbUsuarios['password'] == password):
+        pass
+    
+
